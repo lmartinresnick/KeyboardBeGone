ad684107d6662b16b0b5ef6ac7a02d14f0c241af
Luke Martin-Resnick <63889477+lmartinresnick@users.noreply.github.com>
2024-07-27 14:47:00 -0700

    feat: configure package

--- Sources/KeyboardBeGone/KeyboardBeGone.swift
+++ Sources/KeyboardBeGone/KeyboardBeGone.swift
@@ -1,2 +1,17 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import SwiftUI
+
+public extension View {
+    func hideKeyboard() {
+        KeyboardBeGoneController.hide()
+    }
+}
+
+@MainActor
+public struct KeyboardBeGoneController {
+    public static func hide() {
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    }
+}
